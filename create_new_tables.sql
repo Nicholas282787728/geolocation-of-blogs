ae0fcc11b6f69991c503ad8dc194d4097a810f79
chenyi-macbook-air <chenyi2229@gmail.com>
2015-04-12 14:00:59 -0400

    create smaller table

--- create_new_tables.sql
+++ create_new_tables.sql
@@ -41,24 +41,23 @@ New tables:
 
 create table states_profiles as select state, url from profiles where state is not null and country = 'United States';
 
-
 ALTER TABLE states_profiles CHANGE url profile_url varchar(100);
 ALTER TABLE states_profiles MODIFY profile_url varchar(100) NOT NULL; -- should be no warning
 ALTER TABLE states_profiles MODIFY state text NOT NULL; -- should be no warning
 
-
+/*
 create table state_profile_blog_post_content as
 select SP.state, SP.profile_url, PB.blog_url, BP.post_url
 from states_profiles SP, profiles_blogs PB, blogs_posts BP
 where
 	SP.profile_url = PB.profile_url and
 	PB.blog_url = BP.blog_url;
-
+*/
 
 create table team_blogs as select blog_url from profiles_blogs group by blog_url having count(*) > 1; -- 21 rows affected
+
+/*
 delete from state_profile_blog_post_content where blog_url in (select blog_url from team_blogs); -- 23130 rows affected
-
-
 
 ALTER TABLE state_profile_blog_post_content ADD post_content longtext;
 
@@ -67,10 +66,9 @@ INNER JOIN posts ON state_profile_blog_post_content.post_url = posts.url
 SET state_profile_blog_post_content.post_content = posts.content;
 
 ALTER TABLE state_profile_blog_post_content MODIFY post_content longtext NOT NULL; -- should be no warning
-
+*/
 
 -- alternative solution
-
 
 create table team_posts as
 select BP.post_url
@@ -84,9 +82,3 @@ from states_profiles SP, posts P
 where
 	SP.profile_url = P.author_url and
 	P.url not in (select post_url from team_posts);
-
-
-
-
-
-	
